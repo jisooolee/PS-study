874875c67dabf89e2c51cfdf04409cc98e6eaf9f
ramrami-S <157570857+ramrami-S@users.noreply.github.com>
2024-01-30 14:27:55 +0900

    Update 2805.java
    수업시간에 재 풀이해서 PASS!!

--- Haram/SWEA/D3/2805.java
+++ Haram/SWEA/D3/2805.java
@@ -1,26 +1,16 @@
-//import java.io.FileInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Solution {
 	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("src/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int T = Integer.parseInt(br.readLine());
 		
-		for(int t = 0; t < T; t++) {
+		for(int t = 1; t < T+1; t++) {
 			int profit = 0;
-			
 			int N = Integer.parseInt(br.readLine());
-			
-			if(N == 1) {
-				System.out.printf("#%d %d\n", t+1, Integer.parseInt(br.readLine()));
-				break;
-			}
-			
 			int[][] farm = new int[N][N];
-			
 			
 			for(int i = 0; i < N; i++) {
 				String str = br.readLine();
@@ -28,20 +18,17 @@ public class Solution {
 					farm[i][j] = str.charAt(j) - '0';
 				}
 			}
-		
-			for(int i = 0; i < N/2; i++) {
-				for(int j = N/2-i; j < N/2+i+1; j++) {
+			
+			int cnt = N/2;
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(Math.abs(cnt) > j || Math.abs(cnt) >= N-j) continue;
 					profit += farm[i][j];
 				}
+				cnt--;
 			}
 			
-			for(int i = N/2; i < N; i++) {
-				for(int j = i-N/2; j < N-i+N/2; j++) {
-					profit += farm[i][j];
-				}
-			}
-			
-			System.out.printf("#%d %d\n", t+1, profit);
+			System.out.println("#" + t + " " + profit);
 		}
 	}
 }
