4e64e0d02061161a724e5457ee48c736bdcab0fb
GeonHui2 <022686@naver.com>
2024-02-19 20:10:05 +0900

    Update 17406.java

--- 이건희/backjoon/골드4/17406.java
+++ 이건희/backjoon/골드4/17406.java
@@ -1,101 +1,119 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m, k, res;
-	static int[][] map;
-	static int[][] K;
-	static boolean[] visited;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
-		K = new int[k][3];
-		visited = new boolean[k];
-		for (int i = 0; i <n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j <m; j ++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		for (int i = 0; i < k; i++) {
-			st = new StringTokenizer(br.readLine());
-			K[i][0] = Integer.parseInt(st.nextToken()) -1;
-			K[i][1] = Integer.parseInt(st.nextToken()) -1;
-			K[i][2] = Integer.parseInt(st.nextToken());
-		}
-		res = Integer.MAX_VALUE;
-		
-		permut(0, map);
-		System.out.println(res);
-	}
-	private static void permut(int cnt, int[][] arr) {
-		if (cnt == k) {
-			res = Math.min(res, cal(arr));
-			return;
-		}
-		for (int i = 0; i< k; i++) {
-			if (visited[i]) continue;
-			visited[i] = true;
-			permut(cnt + 1, rotate(arr, K[i][0], K[i][1], K[i][2]));
-			visited[i] = false;
-		}
-	}
-	private static int cal(int[][] arr) {
-		int minSum = Integer.MAX_VALUE;
-		for (int i = 0; i < n; i++) {
-			int sum = 0;
-			for (int j = 0; j < m; j++) {
-				sum += arr[i][j];
-			}
-			minSum = Math.min(minSum, sum);
-		}
-		return minSum;
-	}
-	private static int[][] rotate(int[][] arr, int r, int c, int s) {
-		// TODO Auto-generated method stub
-		int[][] tmp = copy(arr);
-		
-		for (int l = 1; l <= s; l++) {
-			int sR = r - l;
-			int sC = c - l;
-			int eR = r + l;
-			int eC = c + l;
-			int prev = tmp[sR + 1][sC];
-			
-			for (int i = sC; i < eC; i++) {
-				int temp = tmp[sR][i];
-				tmp[sR][i] = prev;
-				prev = temp;
-			}
-			for (int i = sR; i < eR; i++) {
-				int temp = tmp[i][eC];
-				tmp[i][eC] = prev;
-				prev = temp;
-			}
-			for (int i = eC; i > sC; i--) {
-				int temp = tmp[eR][i];
-				tmp[eR][i] = prev;
-				prev = temp;
-			}
-			for (int i = eR; i > sR; i--) {
-				int temp = tmp[i][sC];
-				tmp[i][sC] = prev;
-				prev = temp;
-			}
-		}
-		return tmp;
-	}
-	private static int[][] copy(int[][] arr) {
-		int[][] map2 = new int[n][m];
-		for (int i = 0; i <n; i++) {
-			System.arraycopy(arr[i], 0, map2[i], 0, m);
-		}
-		return map2;
-	}
+    static int N, M, K, result;
+    static int[][] arr;
+    static int[][] copyArray;
+    static int[][] box;
+    static ArrayList<int[]> order;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        result = Integer.MAX_VALUE;
+        arr = new int[N][M];
+        copyArray = new int[N][M];
+        box = new int[K][];
+        order = orderNums(K);
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                copyArray[i][j] = arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken()) - 1;
+            int s = Integer.parseInt(st.nextToken());
+
+            box[i] = new int[]{r, c, s};
+        }
+
+        for (int i = 0; i < order.size(); i++) {
+            arr = copyArr(copyArray);
+
+            for (int j : order.get(i)) {
+                int[] now = box[j];
+                rotate(now[0], now[1], now[2]);
+            }
+
+            for (int[] a : arr) {
+                int cnt = 0;
+                for (int n : a) {
+                    cnt += n;
+                }
+                result = Math.min(cnt, result);
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static void rotate(int R, int C, int S) {
+        for (int s = 1; s <= S; s++) {
+            int x = C - s;
+            int y = R - s;
+            int temp = arr[y][x];
+
+            for (int i = y; i < y + 2 * s; i++) {
+                arr[i][x] = arr[i + 1][x];
+            }
+
+            for (int i = x; i < x + 2 * s; i++) {
+                arr[y + 2 * s][i] = arr[y + 2 * s][i + 1];
+            }
+
+            for (int i = y + 2 * s; i > y; i--) {
+                arr[i][x + 2 * s] = arr[i - 1][x + 2 * s];
+            }
+
+            for (int i = x + 2 * s; i > x; i--) {
+                arr[y][i] = arr[y][i - 1];
+            }
+            arr[y][x + 1] = temp;
+        }
+    }
+
+    private static ArrayList<int[]> orderNums(int n) {
+        ArrayList<int[]> a = new ArrayList<>();
+        boolean[] visited = new boolean[n];
+        int[] permutation = new int[n];
+        per(0, n, permutation, visited, a);
+        return a;
+    }
+
+    private static void per(int index, int n, int[] permutation, boolean[] used, ArrayList<int[]> result) {
+        if (index == n) {
+            result.add(permutation.clone());
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!used[i]) {
+                permutation[index] = i;
+                used[i] = true;
+                per(index + 1, n, permutation, used, result);
+                used[i] = false;
+            }
+        }
+    }
+
+    private static int[][] copyArr(int[][] original) {
+        int[][] copy = new int[original.length][original[0].length];
+        for (int i = 0; i < original.length; i++) {
+            System.arraycopy(original[i], 0, copy[i], 0, original[i].length);
+        }
+        return copy;
+    }
 }
